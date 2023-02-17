2df644e0470b42364ad5ab0e67209aedc16201ea
Ahmed laazouzi <laaz.braid@hotmail.fr>
2023-02-17 11:39:23 -0500

    fix error

--- src/test/java/guru/springframework/springrestclientexamples/SpringRestClientExamplesApplicationTests.java
+++ src/test/java/guru/springframework/springrestclientexamples/SpringRestClientExamplesApplicationTests.java
@@ -1,9 +1,13 @@
 package guru.springframework.springrestclientexamples;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
